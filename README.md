330b05d2b73f6c89c4963d3a014fd30353802888
Matthew Farrugia-Roberts <m@far.in.net>
2024-12-29 21:21:33 +0000

    Package for GitHub

---
+++ README.md
@@ -0,0 +1,183 @@
+Wunderbar: Robust parser for .wandb log files
+=============================================
+
+Robust Python parser for W&B's `.wandb` binary structured log format.
+
+1. Implemented in Python.
+   * Dependencies only on protobuf and the protobuf schema from the wandb SDK.
+   * No dependency on any other internals of the wandb SDK.
+   * No requirement to authenticate with or sync data to the W&B cloud.
+2. The output of the parser is a stream of pure Python objects (dictionaries)
+   rather than protobuf objects.
+3. The parsers can partially recover from encountering formatting errors in the
+   `.wandb` files.
+   * These errors seem to be fairly common my collection of run logs.
+   * They could be caused by data corruption, interrupted writes, or possibly
+     errors in the wandb writers (I am still looking into the latter).
+4. Simple, mostly-'functional'-style implementation with highly-localised state
+   management. Should be easy to understand and build from if you need more
+   features, or to port to other languages if you need more speed.
+
+Warning: While W&B have an incentive to maintain a relatively stable database
+format, this script might stop working if (1) they change the location of their
+protobuf schema or (2) they do eventually change their storage format
+(at either the leveldb log level or the protobuf schema level).
+
+Quick start
+-----------
+
+Install:
+
+```
+pip install git+https://github.com/matomatical/wunderbar.git
+```
+
+Demo:
+
+```
+wunderbar path/to/example-run.wandb
+```
+
+Library:
+
+```
+import wunderbar
+
+PATH = 'path/to/example-run.wandb'
+
+records_or_corruption = wunderbar.parse_filepath(path=PATH)
+records = wunderbar.purify(records_or_corruption)
+for record in records:
+    print(f"Record {record.number} ({record.type})")
+    if record.type == "history": # a call to wandb.log(step, data)
+        step: int = record.data["step"]
+        data: dict = record.data["item"]
+        print(f"{len(data)} metrics logged at {step=}:")
+        for metric, value in data.items():
+            print(f"* {metric}: {value}")
+```
+
+API overview
+------------
+
+Types:
+
+* `LogRecord(number: int, type: RecordType, data: dict, ...)`: an entry in the
+  .wandb log.
+* `RecordType`: the type of the entry.
+  * `"run"`: Various metadata.
+  * `"config"`: Set or change the run configuration.
+  * `"history"`: A call to `wandb.log(step, data)`.
+  * `"files"`: A file was added.
+  * `"stats"`: A sample of system statistics.
+  * `"output_raw"`: Printed to `stdout`.
+  * ... Plus several more.
+* `Corruption(data: bytes, note: str)`: Some un-parse-able binary content, with
+  a brief justification of the problem (e.g. checksum failed).
+
+Commonly-used functions:
+
+* Parsing functions:
+  * `parse_filepath(path: str | pathlib.Path) -> Generator[LogRecord |
+    Corruption]` parses a file at a given path.
+  * `parse_file(file: typing.BinaryIO) -> Generator[LogRecord | Corruption]`
+    parses an already-open file-like object.
+  * `parse_data(data: bytes) -> Generator[LogRecord | Corruption]` parses data
+    already in memory.
+* `purify(g: Generator[LogRecord | Corruption]) -> Generator[LogRecord]`
+  filters out corruption.
+
+See code for full details.
+
+About the .wandb file format
+----------------------------
+
+The `.wandb` files included with every wandb run folder store experiment
+configuration information and metrics in a custom binary 'structured log'
+format. In brief, each fact, file, system statistic, experiment metric or other
+thing logged by W&B is encoded via protobuf and then the data from each logging
+event is appended to the binary file in a format akin to the log files from
+LevelDB (actually the same as the LevelDB log format, except for the choice of
+checksum algorithm, for some reason W&B wanted to use the older CRC32 instead
+of LevelDB's CRC32C).
+
+In more detail:
+
+* ... TODO
+
+Relation to wandb SDK
+---------------------
+
+The wandb SDK / source code includes the following related code.
+
+1. The protobuf schema used for encoding log items
+    (https://github.com/wandb/wandb/blob/main/wandb/proto/wandb_internal.proto).
+2. Code used for writing .wandb databases during an experiment, including in
+   the old Python backend
+    (https://github.com/wandb/wandb/blob/main/wandb/sdk/internal/datastore.py)
+   and the new "core" (Go) backend
+    (https://github.com/wandb/wandb/blob/main/core/pkg/leveldb/record.go).
+   These are used for creating the log files during an experiment.
+3. The same code also supports reading the binary logs, which is done during
+   cloud sync. The wandb CLI also supports printing a string rendering of the
+   contents of the database to stdout via `wandb sync --view --verbose`.
+   Note that the Python backend reader does not support recovering data from
+   partially-corrupted (or partially-improperly-written) .wandb files.
+
+This script is a Python replacement for (3) that draws on (1) but with an
+independent implementation of a decoder for the LevelDB log format that is
+more resilient to errors, and produces pure-Python output objects.
+
+Changes in the wandb SDK
+------------------------
+
+TODO: Describe the issue in wandb-core from before wandb 0.17.6.
+
+https://github.com/wandb/wandb/pull/8088
+
+Roadmap
+-------
+
+Basic functionality
+
+* [x] Divide the file into blocks
+* [x] Divide each block into chunks
+* [x] Aggregate sequences of chunks into raw binary records
+* [x] Use wandb's protobuf schema to parse raw records into protobuf messages
+* [x] Use protobuf's json tools to convert messages into dictionaries
+* [x] Additional post-processing to streamline the dictionaries
+
+Enhanced functionality
+
+* [x] Support streaming (helpful if database is not already in memory)
+* [x] Robust to incomplete reads from unbuffered file-like objects
+* [x] Dedicated types for blocks, chunks, various kinds of records
+* [x] Tracking block/chunk/record numbers and indices
+* [ ] Dedicated types for different kinds of corruption
+* [ ] Structured tracking of corrupt chunk/block/record errors
+* [ ] Improved API (filter corruption by default; parsers for different
+      chunking modes, etc.)
+
+Error tracking and recovery
+
+* [x] Recover from corrupt chunks
+* [x] Recover from corrupt record sequences
+* [x] Recover from corrupt protobuf binary data
+* [ ] Recover from corrupt protobuf record contents
+
+Verification and testing
+
+* [x] Type-check with `mypy`
+* [x] Test parsing on a large log without errors
+  * [x] Fix off-by-one error causing the problem
+* [ ] Test error recovery on a large log with a mysterious padding error
+  * [x] Trace the cause to [a historical bug in wandb core](https://github.com/wandb/wandb/pull/8088)
+  * [ ] Make the parser resilient to this particular bug...
+
+Documentation
+
+* [x] Brief README
+* [ ] Code documentations
+* [ ] Document format
+* [ ] Document format variations
+* [ ] API reference
---
+++ wunderbar.py
@@ -0,0 +1,463 @@
+import collections
+import dataclasses
+import enum
+import io
+import itertools
+import json
+import os
+import pathlib
+import struct
+import sys
+import typing
+import zlib
+
+
+import google.protobuf as protobuf
+import google.protobuf.json_format as protobuf_json
+import wandb.proto.wandb_internal_pb2 as wandb_protobuf
+
+
+# # # 
+# TYPES
+
+
+@dataclasses.dataclass
+class Block:
+    data: bytes
+    # aux
+    number: int # position in sequence of blocks within file
+    index: int  # data = file[file_index:file_index+len(data)]
+
+
+@dataclasses.dataclass
+class Chunk:
+    class Type(enum.IntEnum):
+        FULL    = 1
+        FIRST   = 2
+        MIDDLE  = 3
+        LAST    = 4
+    type: Type
+    data: bytes
+    # aux
+    block: Block    # block within which this chunk was found
+    number: int     # position in sequence of chunks within block
+    index: int      # data = block.data[index:index+len(data)]
+
+
+@dataclasses.dataclass
+class RawRecord:
+    data: bytes
+    # aux
+    number: int     # position in sequence of valid records
+    chunks: tuple[Chunk, ...] # chunks from which this record was assembled
+
+
+if not typing.TYPE_CHECKING:
+    # At runtime, we use the generated class
+    _ProtobufRecord = wandb_protobuf.Record
+else:
+    # At type-check time, use this stub. Needed since W&B doesn't include the
+    # mypy stubs in SDK distributed with pip (though they are in the source)
+    # https://github.com/wandb/wandb/blob/main/wandb/proto/v5/wandb_internal_pb2.pyi)
+    class _ProtobufRecord(protobuf.message.Message):
+        """Stub for wandb.proto.v5.wandb_internal_pb2.Record."""
+
+
+@dataclasses.dataclass
+class ProtobufRecord:
+    record: _ProtobufRecord
+    raw_record: RawRecord
+
+
+RecordType = typing.Literal[
+    "history", "summary", "output", "config", "files", "stats", "artifact",
+    "tbrecord", "alert", "telemetry", "metric", "output_raw", "run", "exit",
+    "final", "header", "footer", "preempting", "noop_link_artifact",
+    "use_artifact", "request",
+]
+
+
+@dataclasses.dataclass
+class LogRecord:
+    number: int             # wandb sequence number (decided at write time)
+    type: RecordType
+    data: dict
+    control: dict | None
+    info: dict | None
+    # aux
+    raw_record: RawRecord
+
+
+@dataclasses.dataclass
+class Corruption:
+    data: bytes
+    note: str
+
+
+class InvalidHeaderException(Exception):
+    """
+    Invalid file header.
+    """
+
+
+# # # 
+# FILE FORMAT AND CHECKSUM VALIDATION MAGIC
+
+
+BLOCK_SIZE = 32768 # bytes (32kiB)
+
+
+HEADER_LEN = 7 # bytes
+
+
+FILE_HEADER = struct.pack(
+    "<4sHB",
+    # magic char[4]
+    b":W&B",
+    # magic short
+    zlib.crc32(bytes("Weights & Biases", "iso8859-1")) & 0xFFFF,
+    # version byte
+    0,
+)
+
+
+CRC_START = {
+    Chunk.Type.FULL:     zlib.crc32(bytes(chr(1), "iso8859-1")) & 0xFFFFFFFF,
+    Chunk.Type.FIRST:    zlib.crc32(bytes(chr(2), "iso8859-1")) & 0xFFFFFFFF,
+    Chunk.Type.MIDDLE:   zlib.crc32(bytes(chr(3), "iso8859-1")) & 0xFFFFFFFF,
+    Chunk.Type.LAST:     zlib.crc32(bytes(chr(4), "iso8859-1")) & 0xFFFFFFFF,
+}
+
+
+# # # 
+# COMPOSED PARSERS
+# 
+# These are where you can enter the file
+
+
+def parse_file(
+    file: typing.BinaryIO,
+) -> collections.abc.Generator[LogRecord | Corruption]:
+    blocks = parse_file_to_blocks(file)
+    chunks = parse_blocks_to_chunks(blocks)
+    raw_records = parse_chunks_to_raw_records(chunks)
+    pb_records = parse_raw_records_to_protobuf_records(raw_records)
+    log_records = parse_protobuf_records_to_log_records(pb_records)
+    yield from log_records
+
+
+def parse_filepath(
+    path: str | pathlib.Path,
+) -> collections.abc.Generator[LogRecord | Corruption]:
+    with open(path, mode='rb', buffering=BLOCK_SIZE) as file:
+        yield from parse_file(file=file)
+
+
+def parse_data(
+    data: bytes,
+) -> collections.abc.Generator[LogRecord | Corruption]:
+    file = io.BytesIO(initial_bytes=data)
+    yield from parse_file(file=file)
+
+
+def purify(
+    g: collections.abc.Generator[LogRecord | Corruption],
+) -> collections.abc.Generator[LogRecord]:
+    for log_or_corruption in g:
+        match log_or_corruption:
+            case LogRecord() as log:
+                yield log
+            case Corruption():
+                pass # discard
+
+
+# # # 
+# INTERNAL LAYER PARSERS
+# 
+# Each works with one layer of abstraction at a time, they are later composed.
+
+
+def parse_file_to_blocks(
+    file: typing.BinaryIO,
+) -> collections.abc.Generator[Block]:
+    # reliable buffered read regardless of type of file we are given
+    def _readn(file: typing.BinaryIO, n: int):
+        data = bytearray(n)
+        i = 0
+        while i < n:
+            new_data = file.read(n - i)
+            if len(new_data) == 0: break
+            data[i:i+len(new_data)] = new_data
+            i += len(new_data)
+        return bytes(data[:i])
+
+    # read and check file header
+    file_header = _readn(file, n=HEADER_LEN)
+    if len(file_header) != HEADER_LEN:
+        raise InvalidHeaderException("File too short.")
+    magic1, magic2, version = struct.unpack("<4sHB", file_header)
+    if magic1 != b":W&B":
+        raise InvalidHeaderException(f"Magic char[4]: expected b':W&B', got {magic1}")
+    if magic2 != 0xBEE1:
+        raise InvalidHeaderException(f"Magic short: expected bee1, got {magic2:x}")
+    if version != 0b00000000:
+        raise InvalidHeaderException(f"Version byte: expected 00, got {version:02x}")
+    
+    # first block (minus header)
+    data = _readn(file, n=BLOCK_SIZE - HEADER_LEN) # IS THIS RIGHT!?
+    yield Block(
+        data=data,
+        number=0,
+        index=HEADER_LEN,
+    )
+    
+    # remaining blocks (loop until read returns 0 bytes)
+    number = 1
+    index = HEADER_LEN + len(data)
+    while data := _readn(file, n=BLOCK_SIZE):
+        yield Block(
+            data=data,
+            number=number,
+            index=index,
+        )
+        number += 1
+        index += len(data)
+
+
+def parse_blocks_to_chunks(
+    blocks: collections.abc.Generator[Block],
+) -> collections.abc.Generator[Chunk | Corruption]:
+    for block in blocks:
+        number = 0
+        index = 0
+        while index <= len(block.data) - HEADER_LEN:
+            # read and validate header:
+            header = block.data[index:index+HEADER_LEN]
+            checksum, data_len, chunk_type = struct.unpack("<IHB", header)
+            try:
+                chunk_type = Chunk.Type(chunk_type)
+            except ValueError:
+                yield Corruption(
+                    data=block.data[index:],
+                    note="bad chunk (type); corrupt rest of block",
+                )
+                break
+            if index + HEADER_LEN + data_len > len(block.data):
+                yield Corruption(
+                    data=block.data[index:],
+                    note="bad chunk (len); corrupt rest of block",
+                )
+                break # skip to next block
+            
+            # read and validate data:
+            data = block.data[index+HEADER_LEN:index+HEADER_LEN+data_len]
+            data_crc32 = zlib.crc32(data, CRC_START[chunk_type]) & 0xFFFFFFFF
+            if checksum != data_crc32:
+                yield Corruption(
+                    data=block.data[index:],
+                    note="bad chunk (crc); corrupt rest of block",
+                )
+                break # skip to next block
+            
+            # if we made it here, it's a valid chunk! advance within block
+            yield Chunk(
+                type=chunk_type,
+                data=data,
+                block=block,
+                number=number,
+                index=index,
+            )
+            number += 1
+            index += HEADER_LEN + data_len
+        else:
+            # if we leave the loop naturally, check the padding
+            if any(block.data[index:]):
+                yield Corruption(
+                    data=block.data[index:],
+                    note="bad chunk padding; corrupt rest of block",
+                )
+
+
+def parse_chunks_to_raw_records(
+    chunks: collections.abc.Generator[Chunk | Corruption],
+) -> collections.abc.Generator[RawRecord | Corruption]:
+    number = 0
+    record_in_progress: list[Chunk] = []
+
+    for chunk_or_corrupt in chunks:
+        match chunk_or_corrupt:
+            case Chunk(type=Chunk.Type.FULL) as chunk:
+                if record_in_progress:
+                    # error: interrupted record-in-progress
+                    yield Corruption(
+                        data=b''.join(c.data for c in record_in_progress),
+                        note="multi-chunk record interrupted by FULL chunk",
+                    )
+                    record_in_progress.clear()
+                # either way, this is a complete single-chunk record!
+                yield RawRecord(
+                    data=chunk.data,
+                    number=number,
+                    chunks=(chunk,),
+                )
+                number += 1
+
+            case Chunk(type=Chunk.Type.FIRST) as chunk:
+                if record_in_progress:
+                    # error: interrupted record-in-progress
+                    yield Corruption(
+                        data=b''.join(c.data for c in record_in_progress),
+                        note="multi-chunk record interrupted by FIRST chunk",
+                    )
+                    record_in_progress.clear()
+                # either way, this is the start of a new multi-chunk record
+                record_in_progress.append(chunk)
+
+            case Chunk(type=Chunk.Type.MIDDLE) as chunk:
+                if record_in_progress:
+                    # continuation of a previously-started multi-part record
+                    record_in_progress.append(chunk)
+                else:
+                    # error: attempt to continue non-existing multi-part record
+                    yield Corruption(
+                        data=chunk.data,
+                        note="MIDDLE chunk without corresponding FIRST chunk",
+                    )
+            
+            case Chunk(type=Chunk.Type.LAST) as chunk:
+                if record_in_progress:
+                    # completion of a previously-started multi-part record!
+                    record_in_progress.append(chunk)
+                    yield RawRecord(
+                        data=b''.join(c.data for c in record_in_progress),
+                        number=number,
+                        chunks=tuple(record_in_progress),
+                    )
+                    number += 1
+                    record_in_progress.clear()
+                else:
+                    # error: attempt to continue non-existing multi-part record
+                    yield Corruption(
+                        data=chunk.data,
+                        note="LAST chunk without corresponding FIRST chunk",
+                    )
+
+            case Corruption() as corruption:
+                if record_in_progress:
+                    # error: interrupted multi-part record
+                    yield Corruption(
+                        data=b''.join(c.data for c in record_in_progress),
+                        note="multi-chunk record interrupted by bad chunk",
+                    )
+                    record_in_progress.clear()
+                # (either way, pass through the corruption itself)
+                yield corruption
+    
+    # another error case: a record remains in progress after final chunk!
+    if record_in_progress:
+        yield Corruption(
+            data=b''.join(c.data for c in record_in_progress),
+            note="multi-chunk record interrupted by eof",
+        )
+        record_in_progress.clear()
+
+
+def parse_raw_records_to_protobuf_records(
+    raw_records: collections.abc.Generator[RawRecord | Corruption],
+) -> collections.abc.Generator[ProtobufRecord | Corruption]:
+    for raw_record_or_corruption in raw_records:
+        match raw_record_or_corruption:
+            case RawRecord(data=data) as raw_record:
+                pb_record = _ProtobufRecord()
+                try:
+                    pb_record.ParseFromString(data)
+                    yield ProtobufRecord(
+                        record=pb_record,
+                        raw_record=raw_record,
+                    )
+                except protobuf.message.DecodeError as e:
+                    yield Corruption(
+                        data=data,
+                        note="protobuf record failed deserialisation",
+                    )
+                    # TODO: track e
+            
+            case Corruption() as corruption:
+                yield corruption
+
+
+def parse_protobuf_records_to_log_records(
+    protobuf_records: collections.abc.Generator[ProtobufRecord | Corruption],
+) -> collections.abc.Generator[LogRecord | Corruption]:
+    for pb_record_or_corruption in protobuf_records:
+        match pb_record_or_corruption:
+            case ProtobufRecord() as pb_record:
+                # convert the protobuf object to a Python dictionary
+                record = protobuf_json.MessageToDict(
+                    pb_record.record,
+                    preserving_proto_field_name=True,
+                )
+                
+                # check the event type
+                record_type = pb_record.record.WhichOneof("record_type")
+                if record_type is None:
+                    # TODO: handle as corruption
+                    assert False, "invalid wandb record, missing type"
+
+                # streamline key/value_json item lists (replace with dicts)
+                for field in ("item", "update", "remove",):
+                    if field in record[record_type]:
+                        mapping = {}
+                        for item in record[record_type][field]:
+                            if 'key' in item:
+                                key = item['key']
+                            else: # 'nested_key' in item:
+                                key = '/'.join(item['nested_key'])
+                            # TODO: handle as corruption
+                            assert key not in mapping, "duplicate item"
+                            value = json.loads(item['value_json'])
+                            mapping[key] = value
+                        record[record_type][field] = mapping
+
+                # assemble
+                yield LogRecord(
+                    type=record_type,
+                    data=record[record_type],
+                    number=record["num"],
+                    control=record["control"] if "control" in record else {},
+                    info=record["info"] if "info" in record else {},
+                    raw_record=pb_record.raw_record,
+                )
+            
+            case Corruption() as corruption:
+                yield corruption
+
+
+# # # 
+# DEMO SCRIPT
+
+
+def main():
+    # parse command line arguments
+    import sys
+    if len(sys.argv) != 2:
+        print("usage: parse.py path/to/run.wandb", file=sys.stderr)
+        sys.exit(1)
+    path = sys.argv[1]
+
+    # entry point
+    print(f"loading wandb database from {path}...")
+    for record_or_corruption in parse_filepath(path=path):
+        match record_or_corruption:
+            case LogRecord() as record:
+                print("Record:", record.type, record.number, record.data)
+            case Corruption() as corruption:
+                print(
+                    "Corruption:",
+                    corruption.note,
+                    f"({len(corruption.data)} bytes)",
+                )
+
+
+if __name__ == "__main__":
+    main()

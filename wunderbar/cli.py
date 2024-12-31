6d5a96ac406cf5189bb72ca016ada43b4397ee91
Matthew Farrugia-Roberts <m@far.in.net>
2024-12-31 14:03:32 +0000

    clean up readme and cli

--- wunderbar/cli.py
+++ wunderbar/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import wunderbar
 
 
@@ -6,6 +7,18 @@ def main():
     # parse command line arguments
     argparser = argparse.ArgumentParser(
         description="Robust .wandb log parser.",
+    )
+    argparser.add_argument(
+        '-p',
+        '--peek',
+        action='store_true',
+        help='Print a one-line preview of each record.',
+    )
+    argparser.add_argument(
+        '-V',
+        '--verbose',
+        action='store_true',
+        help='Print each record in its entirety.',
     )
     argparser.add_argument(
         '-H',
@@ -28,18 +41,34 @@ def main():
     for record_or_corruption in records_or_corruption:
         match record_or_corruption:
             case wunderbar.LogRecord() as record:
-                print(
-                    "Record:",
-                    record.type,
-                    record.number,
-                    f"keys: {','.join(record.data.keys())}",
-                )
-            case wunderbar.Corruption() as corruption:
-                print(
-                    "Corruption:",
-                    corruption.note,
-                    f"({len(corruption.data)} bytes)",
-                )
+                if not args.peek:
+                    print(f"Record: {record.type}")
+                else:
+                    summary = "number={}, contents={}".format(
+                        record.number,
+                        ','.join(record.data.keys()),
+                    )
+                    print(f"Record: {record.type} ({summary})")
+                if args.verbose:
+                    for field in ['number', 'data', 'control', 'info']:
+                        value = record.__getattribute__(field)
+                        print(
+                            " ",
+                            field,
+                            json.dumps(value, indent=2).replace("\n","\n  "),
+                        )
+                    print()
+
+            case wunderbar.Corruption() as c:
+                if not args.peek:
+                    print(f"Corruption: {c.note}")
+                else:
+                    print(f"Corruption: {c.note} ({len(c.data)} bytes)")
+                if args.verbose:
+                    bgrid = [c.data[i:i+16] for i in range(0, len(c.data), 16)]
+                    for i in range(0, len(c.data), 16):
+                        row = c.data[i:i+16]
+                        print(f"{i:>8}:", *[f"{b:02x}" for b in row], sep="  ")
 
 
 if __name__ == "__main__":
